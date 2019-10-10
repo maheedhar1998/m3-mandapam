7a4a35154b645b9e26b8549af0144486d69cdb70
maheedhar1998 <maheedhar1998@hotmail.com>
2019-10-10 13:59:25 -0400

    App Structure

---
+++ m3-mandapam/src/app/home/home.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private qrScan: QRScanner) {}
+
+}
